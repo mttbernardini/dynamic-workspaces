7300e46e828822b24e6babb13ecc499441be54f0
Matteo Bernardini <mat_teobe@yahoo.it>
2020-03-20 17:40:54 +0100

    first kind of working implementation

--- extension.js
+++ extension.js
@@ -1,10 +1,41 @@
 const Meta = imports.gi.Meta;
 
+let ww = {};
+let wse;
+
+function adjustWS() {
+	let i = global.screen.n_workspaces;
+	while (i--) {
+		let ws = global.screen.get_workspace_by_index(i);
+		// if ws has no windows, it should be removed
+		if (ws.list_windows().filter(w => !w.is_on_all_workspaces()).length == 0)
+			global.screen.remove_workspace(ws, global.get_current_time());
+	}
+	// leave one final empty workspace
+	global.screen.append_new_workspace(false, global.get_current_time());
+}
+
+function bindNewWindows() {
+	global.get_window_actors().filter((a) => !(a in ww)).forEach(function(actor) {
+		let win = actor.get_meta_window();
+		let eid = win.connect("workspace-changed", adjustWS);
+		ww[actor] = true;
+		actor.connect("destroy", function() { win.disconnect(eid); delete ww[actor]; });
+	});
+}
+
+// Mandatory Functions //
+
 function init(extensionMeta) {
+
 }
 
 function enable() {
+	// TOFIX there has to do a better way to bind new windows
+	wse = global.screen.connect("window-added", bindNewWindows);
+	bindNewWindows();
 }
 
 function disable() {
+	global.screen.disconnect(wse);
 }
