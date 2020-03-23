26481cf9a5151d5141d3c7f9646bb0680a1aa4f3
Matteo Bernardini <mat_teobe@yahoo.it>
2020-03-23 18:42:36 +0100

    Some rework for reliability

--- extension.js
+++ extension.js
@@ -1,9 +1,7 @@
-const Meta = imports.gi.Meta;
-
-let ww = {};
-let wse;
+let _managed_windows, _wsadd_event, _wsdel_event;
 
 function adjustWS() {
+	// TODO: avoid removing and readding last workspace
 	let i = global.screen.n_workspaces;
 	while (i--) {
 		let ws = global.screen.get_workspace_by_index(i);
@@ -15,27 +13,42 @@ function adjustWS() {
 	global.screen.append_new_workspace(false, global.get_current_time());
 }
 
-function bindNewWindows() {
-	global.get_window_actors().filter((a) => !(a in ww)).forEach(function(actor) {
-		let win = actor.get_meta_window();
-		let eid = win.connect("workspace-changed", adjustWS);
-		ww[actor] = true;
-		actor.connect("destroy", function() { win.disconnect(eid); delete ww[actor]; });
-	});
+function bindWindow(win) {
+	// assumes `win` is not managed
+	let eid = win.connect("workspace-changed", adjustWS);
+	_managed_windows[win] = [win, eid];
+}
+
+function unbindWindow(win) {
+	// assumes `win` is managed
+	if (typeof(win) == "string")
+		win = _managed_windows[win][0];
+	let event_id = _managed_windows[win][1];
+	win.disconnect(event_id);
+	delete _managed_windows[win];
 }
 
 // Mandatory Functions //
 
 function init(extensionMeta) {
-
+	_managed_windows = {};
 }
 
 function enable() {
-	// TOFIX there has to do a better way to bind new windows
-	wse = global.screen.connect("window-added", bindNewWindows);
-	bindNewWindows();
+	_wsadd_event = global.screen.connect("window-added", (_, win) => {
+		if (!(win in _managed_windows))
+			bindWindow(win);
+	});
+	_wsdel_event = global.screen.connect("window-removed", (_, win) => {
+		if (win in _managed_windows)
+			unbindWindow(win);
+	})
+	global.get_window_actors().forEach((actor) => bindWindow(actor.meta_window));
+	adjustWS();
 }
 
 function disable() {
-	global.screen.disconnect(wse);
+	Object.keys(_managed_windows).forEach(unbindWindow);
+	global.screen.disconnect(_wsadd_event);
+	global.screen.disconnect(_wsdel_event);
 }
